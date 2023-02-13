0f9cfe6700f6bcdb32824fd40420a246639105cd
Bolaji06 <joshspencer0603@gmail.com>
2023-02-12 16:49:44 -1200

    add ceasar-cipher

---
+++ ceasar-cipher.js
@@ -0,0 +1,86 @@
+
+const cipherThemeIcon = document.querySelector('.icon-cipher');
+const cipherInput = document.querySelector('#cipher-input');
+const decodeBtn = document.querySelector('.decode-btn');
+let cipherResult = document.getElementById('display-result-cipher');
+const enteredCipherWord = document.querySelector('.entered-word-display-cipher');
+const cipherResetBtn = document.querySelector('.reset-cipher');
+
+cipherThemeIcon.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark-theme')
+});
+
+const CHARACTER_CHART = {
+    'A': 'N',
+    'B': 'O',
+    'C': 'P',
+    'D': 'Q',
+    'E': 'R',
+    'F': 'S',
+    'G': 'T',
+    'H': 'U',
+    'I': 'V',
+    'J': 'W',
+    'K': 'X',
+    'L': 'Y',
+    'M': 'Z',
+    'N': 'A',
+    'O': 'B',
+    'P': 'C',
+    'Q': 'D',
+    'R': 'E',
+    'S': 'F',
+    'T': 'G',
+    'U': 'H',
+    'V': 'I',
+    'W': 'J',
+    'X': 'K',
+    'Y': 'L',
+    'Z': 'M',
+    
+}
+
+function rot13(str){
+        let convertedString = '';
+        if (/^[a-zA-Z?/.,-_@ ]*$/.test(str)){
+            for (let i = 0; i < str.length; i++){
+                const character = str[i].toUpperCase();
+                let convertedCharacter = '';
+
+                if (/[a-z]/i.test(character)){
+                    convertedCharacter = CHARACTER_CHART[character];
+                }
+                else {
+                    convertedCharacter = character;
+                }
+                convertedString += convertedCharacter;
+            }
+        }    
+        
+    return (cipherResult.textContent = convertedString);
+}
+  
+decodeBtn.addEventListener('click', ()=>{
+    let getValue = cipherInput.value;
+    rot13(getValue);
+    enteredCipherWord.textContent = getValue; 
+    
+});
+function clearInput(){
+    let resetInput = document.querySelector('#cipher-input');
+    if (resetInput.value != ''){
+       resetInput.value = '';
+       enteredCipherWord.textContent = '';
+       cipherResult.textContent = '';
+
+    }
+   
+}
+cipherResetBtn.addEventListener('click', ()=>{
+    clearInput();
+    
+})
+
+
+
+
--- index.js
+++ index.js
@@ -1,34 +1,70 @@
 
 const palindromeContainer = document.querySelector('.palindrome-container');
 const romanContainer = document.querySelector('.roman-container');
+const cipherContainer = document.querySelector('.cipher-container');
 const RomanBtn = document.querySelector('.roman');
 const palindromeBtn = document.querySelector('.pali');
+const cipherBtn = document.querySelector('.cipher-btn');
 const themeIcon = document.querySelector('.theme-icon');
 const labels = document.querySelectorAll('label');
 const header = document.querySelector('h1');
-
+const rc = document.querySelector('.rc');
+const cipherRoman = document.querySelector('.cipher-roman');
+const cipherPalindrome = document.querySelector('.cipher-palindrome');
 
 
 themeIcon.addEventListener('click', ()=>{
     document.body.classList.toggle('dark-theme');
 });
-
-
-
 window.addEventListener('load', ()=>{
     palindromeContainer.classList.add('active-palindrome');
 })
-
  
 RomanBtn.addEventListener('click', ()=>{
-    
+    console.log('roman');
     romanContainer.classList.add('active-roman');
     palindromeContainer.classList.remove('active-palindrome');
+    cipherContainer.classList.remove('active-cipher');
+   
+});
+rc.addEventListener('click', ()=>{
+    console.log('roman');
+    romanContainer.classList.add('active-roman');
+    palindromeContainer.classList.remove('active-palindrome');
+    cipherContainer.classList.remove('active-cipher');
    
 });
 palindromeBtn.addEventListener('click', ()=>{
-    romanContainer.classList.remove('active-roman');
+    console.log('palind');
     palindromeContainer.classList.add('active-palindrome');
-})
+    cipherContainer.classList.remove('active-cipher');
+    romanContainer.classList.remove('active-roman');
+});
+cipherPalindrome.addEventListener('click', ()=>{
+    console.log('palind');
+    palindromeContainer.classList.add('active-palindrome');
+    cipherContainer.classList.remove('active-cipher');
+    romanContainer.classList.remove('active-roman');
+});
+
+cipherBtn.addEventListener('click', ()=>{
+    cipherContainer.classList.add('active-cipher');
+    palindromeContainer.classList.remove('active-palindrome');
+    romanContainer.classList.remove('active-roman');
+    
+    console.log('cipher')
+});
+cipherRoman.addEventListener('click', ()=>{
+    cipherContainer.classList.add('active-cipher');
+    palindromeContainer.classList.remove('active-palindrome');
+    romanContainer.classList.remove('active-roman');
+    
+    console.log('cipher')
+});
+
+
+
+
+
 
 
